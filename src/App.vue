e96404c8f74ed7b3629ed7d622d68c27c8b647fa
JoaoReis <joaovitor.olveira16@gmail.com>
2023-07-27 22:31:27 -0300

    Criação do projeto, e instalação do bulma

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Projeto novo portifolio</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
